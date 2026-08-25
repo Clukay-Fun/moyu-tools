ad6e856ac8a00a105f25e7c18da22394f6a0458c
Clukay-Fun <330384604@qq.com>
2026-08-25 23:42:45 +0800

    build(win): add nsis/portable/dir targets for phase-2 startup spike

---
+++ scripts/zip-win-unpacked.mjs
@@ -0,0 +1,17 @@
+import { readFileSync } from 'node:fs'
+import { execFileSync } from 'node:child_process'
+
+const { version } = JSON.parse(readFileSync('package.json', 'utf8'))
+const name = `moyu-dsh-v${version}-windows-x64-unpacked.zip`
+
+execFileSync(
+  'powershell',
+  [
+    '-NoProfile',
+    '-Command',
+    `Compress-Archive -Path release/win-unpacked -DestinationPath release/${name} -Force`
+  ],
+  { stdio: 'inherit' }
+)
+
+console.log(`created release/${name}`)
